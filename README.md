8fcb203a9562f68b4fe1d252ab37d5311bb87bda
Elan Ruusamäe <glen@delfi.ee>
2016-11-21 18:01:25 +0200

    setup slash command
    https://murze.be/2016/07/building-a-slack-bot-with-laravel/

---
+++ README.md
@@ -0,0 +1,5 @@
+# Eventum Slack Slash Command
+
+Eventum Slack Slash Command using (spatie/laravel-slack-slash-command)[spatie/laravel-slack-slash-command] library.
+
+[spatie/laravel-slack-slash-command]: https://murze.be/2016/07/building-a-slack-bot-with-laravel/
--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Eventum Slack Slash Command',
 
     /*
     |--------------------------------------------------------------------------
@@ -178,6 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Spatie\SlashCommand\SlashCommandServiceProvider::class,
     ],
 
     /*
